dda466c27b53e17498c5c552f365946162499deb
Ajinkya <ajinkyagpatil1997@gmail.com>
2026-07-17 18:05:04 +0400

    adding code

---
+++ PythonBAsic/PYthon_07_2026.py
@@ -0,0 +1,102 @@
+
+print("helloo")
+
+#No data Type needed for variables
+a=26
+name= "ajay"
+b=23.78
+
+''''
+#input From user
+name= input("Enter name")
+print(name)
+'''''
+
+'''' ifElse '''''
+n=20
+
+if n%2 == 0:
+    print("EVEN")
+else :
+    print("ODD")
+
+'''' loops '''''
+for i in range(5):
+    print(i)
+
+
+'''' While '''''
+i = 1
+while i <=5:
+    print(i)
+    i= i+1
+"""
+age=input("enter age")
+print(age)
+"""
+
+marks=74
+
+if(marks >= 90):
+    print("Distinction")
+
+elif(marks >=75):
+    print("First Class")
+else:
+    print("fail")
+    
+light= input("light color: ")
+    
+if(light == "red"):
+    print("STop")
+    
+elif(light== "yellow"):
+    print("Look")
+ 
+elif(light == "green"):
+    print("Go")
+ 
+ 
+
+
+
+j=5
+
+print(j**2)
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+    
+   
+
+
+
+
+
+
+    
